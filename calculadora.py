4c733dc59d6e7d11f6c987dda2cb30725fc76008
cedugolden <cedugolden@gmail.com>
2021-01-07 12:28:38 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,21 @@
+numero1 = 0
+numero2 = 0
+oper = ''  
+resultado = 0
+
+while True:
+    numero1 = int(input('Digite um numero: '))
+    oper = input('Digite a operacao: ')
+    numero2 = int(input('Digite o outro numero: '))
+
+    if oper == '+':
+        resultado = numero1 + numero2
+    elif oper == '-':
+        resultado = numero1 - numero2
+    elif oper == 'x':
+        resultado = numero1 * numero2
+    elif oper == '/':
+        resultado = numero1 / numero2
+    else:
+        print('Operacao invalida. :( ')
+    print(str(numero1) + '' + str(oper) + '' + str(numero2) + '=' + str(resultado))
